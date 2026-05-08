7ea10cb1d2b23d74711cbed4397d667c6bd69d07
shambhavi <shambhaviv143@gmail.com>
2026-05-08 23:26:24 +0530

    day 2

--- Remove_Duplicates.java
+++ Remove_Duplicates.java
@@ -8,7 +8,7 @@ public class Remove_Duplicates
     
     public static int solution(int [] arr)
     {
-        if(arr.length==0 || arr==null) return -1;
+        if(arr.length==0) return -1;
         int k=1;
         
         for(int i=1; i<arr.length; i++)
